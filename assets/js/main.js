f04be9a082fd7489ba3719dc1f1c4831f0a4144f
Angelmmc <ammcardenas@hotmail.com>
2024-05-07 10:28:32 -0600

    draw a triangle function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,27 @@
-const canvas = document.getElementById("canvas");
+const canvasSquare = document.getElementById("canvasSquare");
+const canvasTriangle = document.getElementById("canvasTriangle");
 
-draw();
 
-function draw() {
-    const canvas = document.getElementById("canvas");
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-  
-      ctx.fillRect(25, 25, 100, 100);
-      ctx.clearRect(45, 45, 60, 60);
-      ctx.strokeRect(50, 50, 50, 50);
+drawSquare();
+drawTriangle();
+
+function drawSquare() {
+    const ctx = canvasSquare.getContext("2d");
+    if (canvasSquare.getContext) {
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
     }
-  }
-  
+}
+
+function drawTriangle() {
+    if (canvasTriangle.getContext) {
+        const ctx = canvasTriangle.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(75, 50);
+        ctx.lineTo(100, 75);
+        ctx.lineTo(100, 25);
+        ctx.fill();
+    }
+}
+
