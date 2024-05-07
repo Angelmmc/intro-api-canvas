f1c34d9bfcecb8bf5972c94c0fd4d5a9f613e751
Angelmmc <ammcardenas@hotmail.com>
2024-05-07 10:40:49 -0600

    draw lines and move feathers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 const canvasSquare = document.getElementById("canvasSquare");
 const canvasTriangle = document.getElementById("canvasTriangle");
-
+const canvasFeather = document.getElementById("canvasFeather");
+const canvasLines = document.getElementById("canvasLines");
 
 drawSquare();
 drawTriangle();
+drawFeather();
+drawLines();
 
 function drawSquare() {
     const ctx = canvasSquare.getContext("2d");
@@ -18,10 +21,49 @@ function drawTriangle() {
     if (canvasTriangle.getContext) {
         const ctx = canvasTriangle.getContext("2d");
         ctx.beginPath();
-        ctx.moveTo(75, 50);
-        ctx.lineTo(100, 75);
-        ctx.lineTo(100, 25);
+        ctx.moveTo(25, 100);
+        ctx.lineTo(125, 100);
+        ctx.lineTo(75, 15);
         ctx.fill();
     }
 }
+
+function drawFeather() {
+    if (canvasFeather.getContext) {
+      const ctx = canvasFeather.getContext("2d");
+  
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo externo
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false); // Boca (en el sentido de las agujas del reloj)
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Ojo izquierdo
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Ojo derecho
+      ctx.stroke();
+    }
+  }
+
+  function drawLines() {
+    if (canvasLines.getContext) {
+      const ctx = canvasLines.getContext("2d");
+  
+      // Triángulo relleno
+      ctx.beginPath();
+      ctx.moveTo(25, 25);
+      ctx.lineTo(105, 25);
+      ctx.lineTo(25, 105);
+      ctx.fill();
+  
+      // Triángulo contorneado
+      ctx.beginPath();
+      ctx.moveTo(125, 125);
+      ctx.lineTo(125, 45);
+      ctx.lineTo(45, 125);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+  
+  
 
