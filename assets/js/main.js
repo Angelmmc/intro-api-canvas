1c8297f5fd637b87ec1553781f4bf4b1ba9af6df
Angelmmc <ammcardenas@hotmail.com>
2024-05-08 23:30:49 -0600

    path 2d figure added and blue borders eliminated

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@ drawArch();
 drawBezierQuadCurve();
 drawBezierCubicCurve();
 drawCombination();
+drawPath2D();
 
 function drawSquare() {
   const ctx = canvasSquare.getContext("2d");
@@ -219,6 +220,22 @@ function roundedRect(ctx, x, y, width, height, radius) {
   ctx.arcTo(x, y, x, y + radius, radius);
   ctx.stroke();
 }
+
+function drawPath2D() {
+  if (canvasPath2D.getContext) {
+    const ctx = canvasPath2D.getContext("2d");
+
+    const rectangle = new Path2D();
+    rectangle.rect(10, 10, 50, 50);
+
+    const circle = new Path2D();
+    circle.arc(100, 35, 25, 0, 2 * Math.PI);
+
+    ctx.stroke(rectangle);
+    ctx.fill(circle);
+  }
+}
+
 
 
 
