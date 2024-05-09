cbd764d626db8aa1b96ccc9bea952a8d12bfea9b
Angelmmc <ammcardenas@hotmail.com>
2024-05-08 23:23:46 -0600

    bezier cubir curve added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const canvasFeather = document.getElementById("canvasFeather");
 const canvasLines = document.getElementById("canvasLines");
 const canvasArch = document.getElementById("canvasArch");
 const canvasBezierQuadCurve = document.getElementById("canvasBezierQuadCurve");
+const canvasBezierCubicCurve = document.getElementById("canvasBezierCubicCurve");
 
 
 drawSquare();
@@ -12,6 +13,7 @@ drawFeather();
 drawLines();
 drawArch();
 drawBezierQuadCurve();
+drawBezierCubicCurve();
 
 function drawSquare() {
   const ctx = canvasSquare.getContext("2d");
@@ -115,6 +117,24 @@ function drawBezierQuadCurve() {
     ctx.quadraticCurveTo(125, 100, 125, 62.5);
     ctx.quadraticCurveTo(125, 25, 75, 25);
     ctx.stroke();
+  }
+}
+
+function drawBezierCubicCurve() {
+  if (canvasBezierCubicCurve.getContext) {
+    const ctx = canvasBezierCubicCurve.getContext("2d");
+
+    // Ejemplo de curvas cúbicas
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(75, 40);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.fill();
   }
 }
 
